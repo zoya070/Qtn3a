e0832df1b00e6814109523fb306d43af350a6b57
zoya070 <zoyasaifee25@gmail.com>
2023-04-30 21:16:31 +0530

    ok

---
+++ app/src/main/java/com/example/qtn3a/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.qtn3a;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.buttonSend);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, "Hello from App 1!");
+                intent.setType("text/plain");
+                startActivity(Intent.createChooser(intent, "Share via"));
+            }
+        });
+
+    }
+}
